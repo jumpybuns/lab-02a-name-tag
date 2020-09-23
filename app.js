00671e8245887ab1f0f02cdd3aacc7dfd3b35b9f
jumpybuns <ethanpiercepresents@gmail.com>
2020-09-23 16:39:34 -0700

    commit

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 const name = document.getElementById('name');
 const theInput = document.getElementById('user-input');
 const theButton = document.getElementById('my-button');
-const theOtherbutton = document.getElementById('other=button')
-const theOtherinput = document.getElementById('other-input')
 
 theButton.addEventListener('click',() => {
 
